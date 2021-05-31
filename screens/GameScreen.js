a2b1ae5d52b08e055b58ae7d78362332bd99190d
alex kuzin <akuzin20@gmail.com>
2021-05-31 10:48:08 +0300

    basic styling of components

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
 
 const GameScreen = (props) => {
   return (
     <View style={styles.gameScreen}>
-      <Text>The Game Screen</Text>
+      <Text style={{padding: 5}}>The Game Screen</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.title}>Select a number</Text>
+        <TextInput/>
+        <View style={styles.buttonsContainer}>
+          <Button title='Reset' onPress={()=>{}}/>
+          <Button title='Confirm' onPress={()=>{}}/>
+        </View>
+      </View>
     </View>
   )
 }
@@ -14,6 +22,28 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.26,
+    shadowRadius: 6,
+    backgroundColor: 'white',
+    elevation: 5,
+    padding: 10,
+    borderRadius: 10
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-around'
   }
 })
 
